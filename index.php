45006689b58bd708d194e5beed4355939543d76a
sarvesh bhapkar <sarveshbhapkar100@gmail.com>
2024-09-13 17:00:48 +0530

    project setup

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    
+    <title>Discuss</title>
+    <?php include('./client/commonFiles.php') ?>
+
+</head>
+<body>
+    
+<?php include('./client/header.php') ?>
+    
+</body>
+</html>
